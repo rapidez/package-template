152bea65f2b2aa2fdfc14c2647d3293e6b68fffb
indy koning <indy@justbetter.nl>
2026-03-09 10:49:01 +0100

    Rapidez V5 support + codestyle workflows

--- resources/js/package.js
+++ resources/js/package.js
@@ -1,15 +1,17 @@
 // This file will be loaded automatically after installing the package.
 // See: https://github.com/rapidez/rapidez/blob/master/resources/js/app.js
 
-// Vue.component('example', () => import('./components/Example.vue'))
-// -- Or if you don't want it to be lazy loaded:
-// import example from './components/Example.vue'
-// Vue.component('example', example)
+document.addEventListener('vue:loaded', function (event) {
+    const vue = event.detail.vue
 
-document.addEventListener('vue:loaded', function () {
-    // You can access the main Vue instance with "window.app"
+    // import { defineAsyncComponent } from 'vue'
+    // vue.component('example', defineAsyncComponent(() => import('./components/Example.vue')))
+    // -- Or if you don't want it to be lazy loaded:
+    // import example from './components/Example.vue'
+    // vue.component('example', example)
+    // You can access the main Vue instance with "window.app" or "vue"
 
-    // window.app.$on('event-name', () => {
+    // window.$on('event-name', () => {
     //
     // });
 })
